8000ed2fb8bf1ed02e50f22d40ddbc264f26240e
jyotiprakashjena <jyotiprakashjena@IN-CL2Z9K3.Nagarro.local>
2023-04-14 01:21:07 +0530

    Command

---
+++ DesignPatterns/src/com/designpattern/command/TestCommand.java
@@ -0,0 +1,8 @@
+package com.designpattern.command;
+
+public class TestCommand {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
